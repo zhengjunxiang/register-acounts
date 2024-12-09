ff63cc629bac53a9ace911a71cb8cd14b7072e12
zhengjunxiang <zhengjunxiang92@gmail.com>
2024-12-10 00:46:54 +0800

    feat: update

--- register.js
+++ register.js
@@ -1,4 +1,5 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer-extra');
+const stealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const logger = require('./logger'); // 引入日志模块
 const { getTempEmail, checkEmail, getRandomNumber } = require('./utils').default;
@@ -7,6 +8,9 @@ const accounts = JSON.parse(fs.readFileSync('accounts.json', 'utf-8'));
 
 // 延迟函数
 const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
+
+// 启用 stealth 插件
+puppeteer.use(stealthPlugin());
 
 // 自动获取 Chrome 的路径
 async function getChromePath() {
@@ -75,26 +79,41 @@ async function handleSlider(page) {
 
     await page.mouse.move(startX, startY);
     await page.mouse.down();
-    await page.mouse.move(endX, startY, { steps: getRandomNumber(5, 20) });
+    await page.mouse.move(endX, startY, { steps: getRandomNumber(20, 30) });
     await delay(getRandomNumber(300, 600));
     await page.mouse.up();
 
-    await page.waitForFunction(() => {
-      const element = document.getElementById('nc_2_n1z');
-      return element && element.classList.contains('btn_ok');
-    }, { timeout: 30000 });
+    // 每秒检查一次，最多检查 10 次
+    let success = false;
+    for (attempt = 0; attempt < 10; attempt++) {
+      success = await page.evaluate(() => {
+        const successElement = document.querySelector('.nc_wrapper .btn_ok');
+        return successElement !== null;
+      });
+
+      if (success) {
+        console.log(`滑块验证成功 (第 ${attempt + 1} 次检查通过)`);
+        return; // 成功后退出函数
+      }
+
+      console.log(`验证结果未通过，等待重试 (${attempt + 1}/10)...`);
+      await delay(1000); // 每秒检查一次
+    }
+
+    if (!success) {
+      throw new Error('滑块验证未通过，超过 10 次检查限制');
+    }
   } catch (err) {
     logger.error('滑块验证失败: ' + err.message);
     throw err;
   }
 }
-
+// 输入邮箱验证码
 async function handleVerification(page, email) {
   try {
     const verificationCode = await checkEmail(email);
     if (verificationCode) {
       await page.type('input[name="emailVerifyCode"]', verificationCode, { delay: 100 });
-      await delay(600);
     }
   } catch (err) {
     logger.error('处理验证码失败: ' + err.message);
@@ -113,24 +132,33 @@ async function handleVerification(page, email) {
 
   const browser = await puppeteer.launch({
     headless: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-blink-features=AutomationControlled', // 禁用自动化提示
+      '--disable-infobars', // 禁用信息栏（提示 Chrome 正受到控制）
+    ],
     executablePath: chromePath, // 使用检测到的 Chrome 路径
-    userDataDir: './user_data',  // 指定持久化存储目录
-    defaultViewport: { width: 1280, height: 1024 },
+    // userDataDir: './user_data',  // 指定持久化存储目录
+    defaultViewport: { width: 1440, height: 1024 },
   });
 
 
   for (const account of accounts) {
     try {
       let page = await browser.newPage();
-      // await page.goto('https://ug.alibaba.com/', { waitUntil: 'networkidle2' });
-      // await delay(2000)
-      // await page.waitForSelector("input[name='account']")
-      // await page.waitForSelector("input[name='password']")
-      // await page.type("input[name='account']", 'mu7vq3zh6c1@rteet.com', { delay: 100 })
-      // await page.type("input[name='password']", 'Password123', { delay: 100 })
-      // await delay(1000)
-      // await page.click("button.sif_form-submit")
-      // return;
+      // 通过 evaluate 来禁用 WebDriver 检测
+      // await page.evaluateOnNewDocument(() => {
+      //   // 禁用 WebDriver 属性
+      //   Object.defineProperty(navigator, 'webdriver', { get: () => undefined }); // 防止检测到 webdriver
+      //   // 模拟浏览器的其他属性
+      //   Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
+      //   Object.defineProperty(navigator, 'plugins', { get: () => ['Chrome PDF Viewer', 'Native Client'] });
+      // });
+      // const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134';
+
+      // await page.setUserAgent(userAgent);
+
       await navigateToRegistrationPage(page);
 
       const email = await getTempEmail();
@@ -141,24 +169,33 @@ async function handleVerification(page, email) {
       await fillRegistrationForm(page, account, email);
       await handleSlider(page);
 
-      await delay(1000)
       await page.waitForSelector('input[name="memberAgreement"]');
       await page.click('input[name="memberAgreement"]');
+      await delay(1000)
       await page.click('button.RP-form-submit');
 
       await handleVerification(page, email);
 
       // 点击注册
-      await page.click('.RP-modal-item:nth-child(3) button.RP-modal-button');
+      await delay(1000)
+      await page.click('button.RP-modal-button');
       logger.info(`账号 ${email} 注册成功！`);
-      await delay(5000)
-      // 等待登录完成
-      await await page.waitForSelector('.tnh-loggedin .tnh-ma');
-      await delay(5000)
 
+      // 等待登录完成
+      await page.waitForSelector('.tnh-loggedin .tnh-ma');
+      await delay(3000)
       // 跳转到 ug 进行用户设置
       await page.goto('https://ug.alibaba.com/?wx_navbar_transparent=true', { waitUntil: 'networkidle2' });
+      // 登录操作
+      await delay(3000)
+      await page.waitForSelector("input[name='account']")
+      await page.waitForSelector("input[name='password']")
+      await page.type("input[name='account']", email, { delay: 100 })
+      await page.type("input[name='password']", account.password, { delay: 100 })
       await delay(1000)
+      await page.click("button.sif_form-submit")
+      await delay(3000)
+
       await page.waitForSelector('.mb-header-wrapper .mb-header-button');
       await page.click('.mb-header-wrapper .mb-header-button');
 
