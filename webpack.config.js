917baab5c872d2ad699f8b65367cb0fbe62137ff
zhengjunxiang <zhengjunxiang@hzrcb.com>
2024-12-13 18:19:07 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,58 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const nodeExternals = require('webpack-node-externals'); // 用于排除 node_modules
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
   entry: './main.js', // 主入口文件
   output: {
-    filename: 'main.js', // 输出的文件名
-    path: path.resolve(__dirname, 'dist'), // 输出的目录
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   node: {
     __dirname: false,
     __filename: false,
   },
-  // 用于排除外部依赖（避免打包 node_modules）
-  externals: [nodeExternals()],
+  externals: [
+    nodeExternals({
+      allowlist: ['puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth'] // 保留这些库
+    })
+  ],
   resolve: {
     alias: {
-      puppeteer: require.resolve('puppeteer-core'), // 使用 puppeteer-core
+      puppeteer: require.resolve('puppeteer-core'),
     },
-    // 添加支持 .js 和 .json 文件
     extensions: ['.js', '.json'],
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,  // 处理 JavaScript 文件
-  //       exclude: /node_modules/,  // 排除 node_modules
-  //       use: 'babel-loader',  // 使用 babel 进行转译
-  //     },
-  //   ],
-  // },
-  target: 'electron-main', // 指定目标为 Electron 主进程
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  target: 'electron-main',
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()], // 使用 Terser 压缩
+    minimizer: [new TerserPlugin()],
   },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /clone-deep/,
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^bufferutil$/,
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^utf-8-validate$/,
+    }),
+  ]
 };
